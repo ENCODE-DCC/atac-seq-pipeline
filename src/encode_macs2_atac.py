337d6ceb9618d5cbac4414148ac4349e1df52acb
Jin Lee <leepc12@gmail.com>
2018-12-21 02:05:25 -0800

    bug fix for removing overlapping regions in bedgraph

--- src/encode_macs2_atac.py
+++ src/encode_macs2_atac.py
@@ -114,8 +114,9 @@ def macs2(ta, chrsz, gensz, pval_thresh, smooth_win, cap_num_peak,
             fc_bedgraph)
         run_shell_cmd(cmd4)
       
-        cmd5 = 'LC_COLLATE=C sort -k1,1 -k2,2n {} | awk \'BEGIN{{OFS="\\t"}}{{if (NR>1 && prev_chr==$1 && prev_chr_e<=$2) {{print $0}}; prev_chr=$1; prev_chr_e=$3;}}\' > {}'
-        cmd5 = cmd5.format(
+        cmd5 = 'LC_COLLATE=C sort -k1,1 -k2,2n {} | '
+            'awk \'BEGIN{{OFS="\\t"}}{{if (NR==1 || NR>1 && (prev_chr!=$1 || prev_chr==$1 && prev_chr_e<=$2)) '
+            '{{print $0}}; prev_chr=$1; prev_chr_e=$3;}}\' > {}'.format(
             fc_bedgraph,
             fc_bedgraph_srt)
         run_shell_cmd(cmd5)
@@ -149,8 +150,9 @@ def macs2(ta, chrsz, gensz, pval_thresh, smooth_win, cap_num_peak,
             pval_bedgraph)
         run_shell_cmd(cmd8)
 
-        cmd9 = 'LC_COLLATE=C sort -k1,1 -k2,2n {} | awk \'BEGIN{{OFS="\\t"}}{{if (NR>1 && prev_chr==$1 && prev_chr_e<=$2) {{print $0}}; prev_chr=$1; prev_chr_e=$3;}}\' > {}'
-        cmd9 = cmd9.format(
+        cmd9 = 'LC_COLLATE=C sort -k1,1 -k2,2n {} | '
+            'awk \'BEGIN{{OFS="\\t"}}{{if (NR==1 || NR>1 && (prev_chr!=$1 || prev_chr==$1 && prev_chr_e<=$2)) '
+            '{{print $0}}; prev_chr=$1; prev_chr_e=$3;}}\' > {}'.format(
             pval_bedgraph,
             pval_bedgraph_srt)
         run_shell_cmd(cmd9)
