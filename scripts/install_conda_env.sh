7fef874a55d44448ba832804b360f09e64b98cec
Jin wook Lee <leepc12@gmail.com>
2022-02-24 14:12:11 -0800

    fix conda ncurses issue

--- scripts/install_conda_env.sh
+++ scripts/install_conda_env.sh
@@ -14,9 +14,12 @@ conda create -n encode-atac-seq-pipeline-macs2 --file ${SH_SCRIPT_DIR}/requireme
 conda create -n encode-atac-seq-pipeline-spp --file ${SH_SCRIPT_DIR}/requirements.spp.txt \
   --override-channels -c r -c bioconda -c defaults -y
 
-# adhoc fix for https://github.com/ENCODE-DCC/chip-seq-pipeline2/issues/259
-# force-install readline 6.2 from conda-forge (ignoring dependencies)
-conda install -n encode-atac-seq-pipeline-spp --no-deps --no-update-deps -y readline==6.2 -c conda-forge
+# adhoc fix for the following issues:
+# - https://github.com/ENCODE-DCC/chip-seq-pipeline2/issues/259
+# - https://github.com/ENCODE-DCC/chip-seq-pipeline2/issues/265
+# force-install readline 6.2, ncurses 5.9 from conda-forge (ignoring dependencies)
+conda install -n encode-atac-seq-pipeline-spp --no-deps --no-update-deps -y \
+  readline==6.2 ncurses==5.9 -c conda-forge
 
 conda create -n encode-atac-seq-pipeline-python2 --file ${SH_SCRIPT_DIR}/requirements.python2.txt \
   --override-channels -c conda-forge -c bioconda -c defaults -y
